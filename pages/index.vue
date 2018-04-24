2b61340dae06177f58c7a2abff430255e5e36328
Even Westvang <even@bengler.no>
2018-04-24 22:38:38 +0200

    Add background image, tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,15 @@
         </a>
       </h1>
       <h2 class="subtitle">
-        A front-end based on the sanity e-commerce example studio,
-        written in <a href="https://vuejs.org/">vuejs</a> and <a href="https://nuxtjs.org/">nextjs</a>
+        A minimal front-end for the sanity e-commerce example schema,
+        written on <a href="https://vuejs.org/">vue.js</a> and <a href="https://nuxtjs.org/">nuxt.js</a> with a client-side shopping cart from <a href="https://snipcart.com/">Snipcart</a>.
       </h2>
     </div>
   </section>
 </template>
 
 <script>
+
 import AppLogo from '~/components/AppLogo'
 
 export default {
@@ -26,12 +27,25 @@ export default {
 </script>
 
 <style scoped>
-  .title {
-    font-size: 10vw; 
+
+  section {
+    /* background: url('/el-alce-web-544198-unsplash.jpg'); */
+    background: url('/adam-birkett-216660-unsplash.jpg');
+    background-size: cover;
   }
+
   .title, .subtitle {
     text-align: center;
     padding: 1em;
+  }
+
+  .title {
+    font-size: 10vw; 
     padding-bottom: 0.5em;
   }
+
+  .subtitle {
+    padding-bottom: 3em;
+  }
+
 </style>
