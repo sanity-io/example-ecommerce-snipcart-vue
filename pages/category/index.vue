34a06f34b9c003edbf9597b8149efab4822200f9
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-05-02 16:31:56 +0200

    removing sub categories from category page

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -9,13 +9,6 @@
           <router-link :to="'/category/' + category.slug.current">
             {{category.title}}
           </router-link>
-          <ul v-if="category.categories" class="sub-categories">
-            <li v-for="subCategory in category.categories" v-bind:key="category._id + subCategory._id"  v-if="subCategory.slug">
-              <router-link :to="'/category/' + category.slug.current + '/' + subCategory.slug.current">
-                {{category.title}}
-              </router-link>
-            </li>
-          </ul>
         </li>
       </ul>
     </div>
