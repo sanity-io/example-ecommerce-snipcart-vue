1df155b2289cc49feb551298937047e47ef7c8b0
Bjørge Næss <bjoerge@gmail.com>
2018-05-02 19:23:47 +0200

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,18 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
+    'eslint:recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    semi: [2, 'never'],
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': 'error'
+  }
+};
