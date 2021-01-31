e70d6c8b752f9a5de4b1b85bfebc1b9fe13ec010
mornir <jeromepott@hotmail.com>
2021-01-31 10:17:52 +0100

    Add target static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,54 @@
-import client from "./sanity.js"
+import client from './sanity.js'
 
 export default {
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
   /*
    ** Headers of the page
    */
   head: {
-    title: "ecommerce-frontend",
+    title: 'ecommerce-frontend',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: "Sanity frontend example for E-commerce in Vue.js"
-      }
+        hid: 'description',
+        name: 'description',
+        content: 'Sanity frontend example for E-commerce in Vue.js',
+      },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       // Snipcart styling
       {
-        href: "https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css",
-        type: "text/css",
-        rel: "stylesheet"
-      }
+        href: 'https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css',
+        type: 'text/css',
+        rel: 'stylesheet',
+      },
     ],
     script: [
       // jQuery. Only needed for snipcart
       {
-        src: "https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"
+        src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
       },
       // Snipcart js
       {
-        src: "https://cdn.snipcart.com/scripts/2.0/snipcart.js",
-        id: "snipcart",
-        "data-autopop": "false",
+        src: 'https://cdn.snipcart.com/scripts/2.0/snipcart.js',
+        id: 'snipcart',
+        'data-autopop': 'false',
         // Change me. Read more at http://snipcart.com
-        "data-api-key":
-          "ODRkNmJhZDktOTk5YS00Y2Y1LTk5Y2ItMTkzNTlkZTYxNzhmNjM2NTk1NTI2OTgyMTc1MTUy"
-      }
-    ]
+        'data-api-key':
+          'ODRkNmJhZDktOTk5YS00Y2Y1LTk5Y2ItMTkzNTlkZTYxNzhmNjM2NTk1NTI2OTgyMTc1MTUy',
+      },
+    ],
   },
   /*
    ** Customize the progress bar color
    */
-  loading: { color: "#3B8070" },
+  loading: { color: '#3B8070' },
   /*
    ** Load categories and vendors
    */
@@ -51,13 +56,13 @@ export default {
   /*
    ** Global CSS
    */
-  css: ["~/css/global.css"],
+  css: ['~/css/global.css'],
   /*
    ** Build configuration
    */
   build: {},
   generate: {
-    routes: async function() {
+    routes: async function () {
       const paths = await client.fetch(`{
         "product": *[_type == "product"].slug.current,
         "category": *[_type == "category"].slug.current,
@@ -66,10 +71,10 @@ export default {
       return Object.keys(paths).reduce(
         (acc, key) => [
           ...acc,
-          ...paths[key].reduce((acc, curr) => [...acc, `${key}/${curr}`], [])
+          ...paths[key].reduce((acc, curr) => [...acc, `${key}/${curr}`], []),
         ],
         []
       )
-    }
-  }
+    },
+  },
 }
