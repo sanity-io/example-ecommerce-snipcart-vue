d33612b41044cf252473ed4ff23e3ca9522ea58f
mornir <jeromepott@hotmail.com>
2021-01-31 10:08:05 +0100

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
-    'eslint:recommended',
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    "@nuxtjs",
+    "prettier",
+    "prettier/vue",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
   ],
   // required to lint *.vue files
-  plugins: ['vue', 'prettier'],
+  plugins: ["prettier"],
   // add your custom rules here
   rules: {
-    semi: [2, 'never'],
-    'vue/max-attributes-per-line': 'off',
-    'prettier/prettier': 'error'
-  }
-};
+    "no-console": "off",
+  },
+}
