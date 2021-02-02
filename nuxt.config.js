a2a568b8766a4ea12b739804ccf6c58590e897b6
mornir <jeromepott@hotmail.com>
2021-02-02 18:22:55 +0100

    Provide modern build for evergreen browsers

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+
+  // https://nuxtjs.org/api/configuration-modern/
+  modern: process.env.NODE_ENV === 'production',
   /*
    ** Headers of the page
    */
